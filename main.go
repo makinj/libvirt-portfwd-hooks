44d9104d3595ca977dc3837070c33ae44d3ed101
Joshua Makinen <makinj64@gmail.com>
2019-12-08 22:55:23 +0000

    creates the most basic libvirt hook in golang

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	//Setup logs
+	f, err := os.OpenFile("/tmp/libvirt-portfwd-hooks.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	//Verify arguments
+	if len(os.Args) != 3 {
+		log.Fatal(fmt.Sprintf("Usage: %s <domain> <action>", os.Args[0]))
+	}
+
+	//Get arguments
+	domain := os.Args[1]
+	action := os.Args[2]
+
+	log.Printf(domain)
+	log.Printf(action)
+
+}
