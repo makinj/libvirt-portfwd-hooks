1e9988f55848e50dafd531f1a58aff95304f1536
Joshua Makinen <makinj64@gmail.com>
2019-12-09 00:11:07 +0000

    forces links in install scripts

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ mkdir -p "${LIBVIRT_HOOKS_DIR}"
 
 sudo cp libvirt-portfwd-hooks "${LIBVIRT_HOOKS_DIR}"
 
-sudo ln -s ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/qemu
-sudo ln -s ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/network
-sudo ln -s ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/lxc
+sudo ln -fs ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/qemu
+sudo ln -fs ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/network
+sudo ln -fs ${LIBVIRT_HOOKS_DIR}/hooks ${LIBVIRT_HOOKS_DIR}/lxc
