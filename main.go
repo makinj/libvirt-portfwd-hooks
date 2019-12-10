32c6a1e1643c25de8b01f5e48de3224e71c003ff
Joshua Makinen <makinj64@gmail.com>
2019-12-10 02:51:43 +0000

    fixes up some of how iptables work

--- main.go
+++ main.go
@@ -12,27 +12,26 @@ import (
 	"github.com/coreos/go-iptables/iptables"
 )
 
-type HostPort struct {
-	Interface string
-	Ip        string
-	Ports     string
-}
-
 type PortForward struct {
-	Protocol    string
-	Source      HostPort
-	Destination HostPort
+	Protocol string
+
+	Ports string
+
+	SourceInterface string
+	BridgeInterface string
+
+	OriginalIp    string
+	DestinationIp string
 }
 
 type Action string
 
 func (portfwd PortForward) HandleEvent(action Action) error {
-	log.Printf("forwarding %s port %s to %s", portfwd.Protocol, portfwd.Source.Ports, portfwd.Destination.Ip)
+	log.Printf("forwarding %s port %s to %s", portfwd.Protocol, portfwd.Ports, portfwd.DestinationIp)
 
-	//iptables -t nat -A PREROUTING -p tcp --dport 8443 -i enp0s31f6 -j DNAT --to-ports 8443 --to 10.0.64.40
-	natrulespec := []string{"-p", portfwd.Protocol, "--dport", portfwd.Source.Ports, "-i", portfwd.Source.Interface, "-j", "DNAT", "--to", portfwd.Destination.Ip}
+	natrulespec := []string{"-p", portfwd.Protocol, "--dport", portfwd.Ports, "-i", portfwd.SourceInterface, "-d", portfwd.OriginalIp, "-j", "DNAT", "--to", portfwd.DestinationIp}
 
-	filterrulespec := []string{"-p", portfwd.Protocol, "--dport", portfwd.Destination.Ports, "-d", portfwd.Destination.Ip, "-i", portfwd.Source.Interface, "-o", portfwd.Destination.Interface, "-m", "state", "--state", "NEW,ESTABLISHED,RELATED", "-j", "ACCEPT"}
+	filterrulespec := []string{"-p", portfwd.Protocol, "--dport", portfwd.Ports, "-d", portfwd.DestinationIp, "-i", portfwd.SourceInterface, "-o", portfwd.BridgeInterface, "-m", "state", "--state", "NEW,ESTABLISHED,RELATED", "-j", "ACCEPT"}
 
 	ipt, err := iptables.New()
 	if err != nil {
